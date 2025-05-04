2179456602c55fc55bfc76cee69b148c89d22e3b
Elgaml <mahmoudelgmal991@gmail.com>
2025-05-04 23:20:27 +0300

    Add to Cart: finish Add to Cart

--- lib/features/products_feature/presentation/pages/products_screen.dart
+++ lib/features/products_feature/presentation/pages/products_screen.dart
@@ -10,7 +10,9 @@ import 'package:sadeem_task/config/routes/routes.dart';
 import 'package:sadeem_task/core/cache/storage_token.dart';
 import 'package:sadeem_task/core/di/config.dart';
 import 'package:sadeem_task/core/utils/app_color.dart';
+import 'package:sadeem_task/core/utils/component/dialog/dilog_utils.dart';
 import 'package:sadeem_task/core/utils/component/white_contanier.dart';
+import 'package:sadeem_task/features/cart/presentation/cubit/cart_cubit.dart';
 import 'package:sadeem_task/features/products_feature/presentation/cubit/product_cubit.dart';
 import 'package:sadeem_task/features/products_feature/presentation/widgets/product_grid_builder.dart';
 import 'package:sadeem_task/features/products_feature/presentation/widgets/product_item.dart';
@@ -21,19 +23,30 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return Scaffold(
       backgroundColor: AppColor.backgroundColor,
       body: Padding(
-        padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+        padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 50),
-            WelcomeSection(),
-            Gap(16),
-            Expanded(
-              child: WhiteContanier(
-                child: ProductGridBuilder(),
+            const SizedBox(height: 50),
+            const WelcomeSection(),
+            const Gap(16),
+            BlocListener<CartCubit, CartState>(
+              listener: (context, state) {
+                if (state is AddToCartLoadingState) {
+                  DialogUtils.showLoadingDialog(context);
+                } else if (state is AddToCartErrorState) {
+                  context.pop();
+                  DialogUtils.showErrorDialog(context, state.error);
+                } else if (state is AddToCartSucssessState) {
+                  context.pop();
+                  DialogUtils.showSuccesLoading(context, "Add Successfully");
+                }
+              },
+              child: const Expanded(
+                child: WhiteContanier(child: ProductGridBuilder()),
               ),
             ),
           ],
