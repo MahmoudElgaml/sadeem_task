4b0020c8cffda8f47820822be3581d6c4108f504
Elgaml <mahmoudelgmal991@gmail.com>
2025-05-04 07:53:38 +0300

    add animate do package - add animation to every list and produt detail

--- lib/features/products_feature/presentation/widgets/welcome_section.dart
+++ lib/features/products_feature/presentation/widgets/welcome_section.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
@@ -18,48 +19,53 @@ class WelcomeSection extends StatelessWidget {
     LoginResponseEntity? userData = getIt<HiveManager>().retrieveUserData(
       HiveKeys.userBox,
     );
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Welcome ",
-                    style: AppStyle.textRegular16(context),
-                  ),
-                  TextSpan(
-                    text: userData?.name ?? " No Name",
-                    style: AppStyle.textBold20(context),
-                  ),
-                ],
+    return FadeInUp(
+      curve: Curves.linearToEaseOut,
+      duration: const Duration(milliseconds: 900),
+      delay: const Duration(seconds: 1),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Welcome ",
+                      style: AppStyle.textRegular16(context),
+                    ),
+                    TextSpan(
+                      text: userData?.name ?? " No Name",
+                      style: AppStyle.textBold20(context),
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(8),
+              Text('New Cairo , Egypt', style: AppStyle.textMedium16(context)),
+            ],
+          ),
+          const Spacer(),
+          Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColor.whiteColor,
+            ),
+            padding: const EdgeInsets.all(8),
+            child: ClipOval(
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: userData?.image ?? "",
+                width: 50,
+                height: 50,
+                errorWidget:
+                    (context, url, error) => const Icon(Icons.error, size: 50),
               ),
             ),
-            const Gap(8),
-            Text('New Cairo , Egypt', style: AppStyle.textMedium16(context)),
-          ],
-        ),
-        const Spacer(),
-        Container(
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: AppColor.whiteColor,
           ),
-          padding: const EdgeInsets.all(8),
-          child: ClipOval(
-            child: CachedNetworkImage(
-              fit: BoxFit.cover,
-              imageUrl: userData?.image ?? "",
-              width: 50,
-              height: 50,
-              errorWidget:
-                  (context, url, error) => const Icon(Icons.error, size: 50),
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
